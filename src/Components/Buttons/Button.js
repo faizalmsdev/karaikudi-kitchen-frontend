0ab5ff1675c146c5ae51a66e287721e44c9f46c6
Mohamed Faizal <faizalmohamed.vi@gmail.com>
2024-02-01 17:53:51 +0530

    button added

--- src/Components/Buttons/Button.js
+++ src/Components/Buttons/Button.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Button = () => {
+    const handleOrderNow = ()=>{
+        console.log('order now clicked')
+    }
     return (
-        <button type='button' className='bg-primary w-22 p-1 rounded-md text-sm text-background px-4 py-2 hover:bg-secondary hover:text-text font-semibold'>
+        <button type='button' className='bg-primary w-22 p-1 rounded-md text-sm text-background px-4 py-2 hover:bg-secondary hover:text-text font-semibold' onClick={handleOrderNow}>
             Order Now
         </button>
     )
