e28e03d23a703daee7d6e1c1c7e485bea057609c
Mohamed Faizal <faizalmohamed.vi@gmail.com>
2024-02-08 17:18:30 +0530

    Contact us page initialized

--- src/Pages/contactus/Contact.js
+++ src/Pages/contactus/Contact.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import Layout from "../../Layout/index"
+import Container from '../../Layout/Container';
 
 const Contact = () => {
     return (
         <div>
             <Layout activePage={"contact"}>
-                <h1>Contact Me</h1>
+                <Container>
+                    <div className='flex flex-col  '>
+                        <h1 className='text-center my-14'>Feel Free To Contact Us</h1>
+
+                        <div className='flex justify-around bg-secondary py-10 h-[20rem] mb-28'>
+                            <div className='flex flex-col justify-between'>
+
+                                <div>
+                                    <span>
+                                        icon
+                                    </span>
+                                    <span>
+                                        address
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        telephone
+                                    </span>
+                                    <span>
+                                        number
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        email
+                                    </span>
+                                    <span>
+                                        mail id
+                                    </span>
+                                </div>
+
+                            </div>
+
+                            <div>
+                                map
+                            </div>
+                        </div>
+                    </div>
+                </Container>
             </Layout>
         </div>
     )
